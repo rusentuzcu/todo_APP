0eee82698655f99f28446fb3b5897b6e4946eaf6
rusen <rusentuzcu@yahoo.com>
2022-11-30 12:56:36 +0000

    initial commit

---
+++ functions.py
@@ -0,0 +1,16 @@
+def get_todos(file_path="todos.txt"):
+    """ Read a text file and return the list of to-do items."""
+    with open(file_path, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, file_path='todos.txt'):
+    """ Write the to-do items list in the text file."""
+    with open(file_path, 'w') as file:
+        file.writelines(todos_arg)
+
+if __name__ == "__main__":
+    print("hello")
+    print(get_todos())
+    
